14963c508b6bb0a740490dad5f518bd66ca102df
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-02-13 21:23:02 -0300

    remove class

--- Objetivos_Strategy.js
+++ Objetivos_Strategy.js
@@ -18,39 +18,9 @@ class ConquistarEuropaObjective extends InterStrategy {
   }
 }
 
-// Contexto
-class Player {
-  constructor(objectiveStrategy) {
-    this.objectiveStrategy = objectiveStrategy;
-  }
-
-  // Executar o objetivo atual
-  executeCurrentObjective() {
-    return this.objectiveStrategy.executeObjective();
-  }
-
-  // Escolher objetivo
-  setObjectiveStrategy(objectiveStrategy) {
-    this.objectiveStrategy = objectiveStrategy;
-  }
-}
-
-
-/*
-console.log(objetivo1);const player1 = new Player(new ConquistarEuropaObjective());
-const objetivo1 = player1.executeCurrentObjective();
-
-console.log(objetivo1);
-
-player1.setObjectiveStrategy(new DerrotarPlayerAzulObjective());
-const objetivo2 = player1.executeCurrentObjective();
-
-console.log(objetivo2);
-*/
 
 module.exports = {
   InterStrategy,
   DerrotarPlayerAzulObjective,
   ConquistarEuropaObjective,
-  Player,
 };
